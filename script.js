253401d45a6dbeb20eb5f1dca6b033de9e7dc2d6
kiramat3344 <kiramat0323@gmail.com>
2024-09-20 10:55:57 +0500

    exercise no 6

---
+++ script.js
@@ -0,0 +1,72 @@
+// exercise no 6 
+
+// let str = "hello     world";
+// let cleanedStr = str.replace(/\s+/g, ' ').trim();
+// console.log(cleanedStr);
+
+
+// question no 2
+// using .slice method 
+
+// const str = "Hello World";
+// const result = str.slice(5, 11);
+// console.log(result); // Output: " World"
+
+
+// question no 3
+// using a .slice method
+
+// const str = "Hello World";
+// const result = str.slice(0, 5);
+// console.log(result); // Output: "Hello"
+
+// question no 4
+// using .split method
+
+// const str = "hello-world";
+// const result = str.split('-');
+// console.log(result); // Output: ["hello", "world"]
+
+// question no 5 
+// using a .join method 
+
+// const array = ["hello", "world"];
+// const result = array.join(" ");
+// console.log(result);
+
+// question no 6 
+// using a .indexof method 
+
+// const str = "hello world";
+// const index = str.indexOf("o");
+// console.log(index);
+
+// question no 7
+// using a .lastindexof method
+
+// const str = "hello world world";
+// const lastIndex = str.lastIndexOf("world");
+// console.log(lastIndex);
+
+// question no 8
+// using a .startwith method 
+
+// let str = "world";
+// let result = str.startsWith("wor");
+
+// console.log(result); // This will output: true
+
+// question no 9
+// using a .endwith method 
+
+// let str = "Hello World";
+// let result = str.endsWith("rld");
+// console.log(result);// this will out put is: true
+
+// question no 10
+// using a .includes method 
+
+// let str = "hello";
+// let result = str.includes("o");
+
+// console.log(result); // This will output: true
